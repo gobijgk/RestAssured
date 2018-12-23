fcc16cff22bc97695cdf554fb65f9e6c034c5ce7
gobijgk <gobijgk@gmail.com>
2018-12-23 09:47:05 +0530

    Basic Block code is ready

--- RestAssuredTest01/src/test/java/com/gobi/Rest/RestAssuredBlocks.java
+++ RestAssuredTest01/src/test/java/com/gobi/Rest/RestAssuredBlocks.java
@@ -1,7 +1,12 @@
 package com.gobi.Rest;
 
 import  io.restassured.matcher.RestAssuredMatchers;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
 
+import javax.mail.internet.ContentType;
+
+import static org.hamcrest.Matchers.everyItem;
 //import com.aventstack.extentreports.gherkin.model.Given;
 
 //import com.aventstack.extentreports.gherkin.model.Given;
@@ -13,14 +18,30 @@ import io.restassured.RestAssured;
 public class RestAssuredBlocks {
 	public static void main(String[] args) {
 //		Base Url
-		RestAssured.baseURI="http://www.appdomain.com";
+		/*RestAssured.baseURI="https://petstore.swagger.io";
 		RestAssured.given().
-		                 param("size", "20").
-		                 param("page", "5").
-		             when().
-		                 get("/users").
-		             then().
-		             assertThat().statusCode(200);
+		                 param("status", "available").
+		                 when().
+		                 get("/v2/pet/findByStatus").
+		                 then().
+		                 assertThat().statusCode(200).and().contentType("application/json").and()
+		                 .body("photoUrls",everyItem( hasItem("string")));*/
+RestAssured.baseURI="https://maps.googleapis.com";
+		
+RestAssured.given().
+		       param("location","-33.8670522,151.1957362").
+		       param("radius","500").
+		       param("key","AIzaSyDIQgAh0B4p0SdyYkyW8tlG-y0yJMfss5Y").
+		       when().
+		       get("/maps/api/place/nearbysearch/json").
+		       then().assertThat().statusCode(200).and().contentType("application/json").and().
+		       body("results[0].name",equalTo("Sydney")).and().
+		       body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM")).and()
+		       .header("server", "scaffolding on HTTPServer2")
+		       //.and().body("results.photos.scope", equalTo("GOOGLE"))
+		       
+		       ;
+		       //.body("results[0].geometry.location.lat",equalTo("-33.86882"));
 	}
 
 }
